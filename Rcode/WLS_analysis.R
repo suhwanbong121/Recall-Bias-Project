2ae9ad6e9b6fc5659b51b192bf61c025d7cf7215
suhwanbong121 <suhwanbong121@gmail.com>
2023-05-02 10:17:13 +0900

    Updated Rcodes

--- Rcode/WLS_analysis.R
+++ Rcode/WLS_analysis.R
@@ -97,52 +97,21 @@ for(i in 1:length(eta0.seq)){
 ########################################################
 ### Figure 1
 
-## Bounds for the ATE - Stratification
-## Define a function to calculate a bound
-bound = function(count.mat, delta){
-  a.star = count.mat[,1] # exposed & case
-  b.star = count.mat[,2] # exposed & control
-  c.star = count.mat[,3] # unexposed & case
-  d.star = count.mat[,4] # unexposed & control
-  n.vec = a.star + b.star + c.star + d.star
-  
-  p1U = a.star/(a.star + b.star*(1-delta))
-  p1L = a.star/(a.star + b.star/(1-delta))
-  p0L = (c.star-delta*a.star/(1-delta))/(c.star + d.star - delta*a.star/(1-delta))
-  p0U = (c.star)/(c.star + d.star - delta*b.star/(1-delta))
-  
-  pU = p1U-p0L
-  pL = p1L-p0U
-  
-  UB = sum(n.vec*pU)/sum(n.vec)
-  LB = sum(n.vec*pL)/sum(n.vec)
-  
-  return (c(LB,UB))
-}
-
-## Select delta sequence and calculate the bounds
-delta.seq = seq(0,0.5, by = 0.01)
-l = length(delta.seq)
-bound.prop.U = bound.prog.U = bound.block.U = rep(NA, length(delta.seq))
-bound.prop.L = bound.prog.L = bound.block.L = rep(NA, length(delta.seq))
-for(i in 1:length(delta.seq)){
-  bound.prop.L[i] = bound(count.mat.prop, delta.seq[i])[1]
-  bound.prop.U[i] = bound(count.mat.prop, delta.seq[i])[2]
-  bound.prog.L[i] = bound(count.mat.prog, delta.seq[i])[1]
-  bound.prog.U[i] = bound(count.mat.prog, delta.seq[i])[2]
-  bound.block.L[i] = bound(count.mat.block, delta.seq[i])[1]
-  bound.block.U[i] = bound(count.mat.block, delta.seq[i])[2]
-}
-
-## Bounds for the ATE - ML
+## Bounds for the ATE
 ## Lower bound at eta0 = delta and eta1 = 0
 ## Upper bound at eta0 = 0 and eta1 = delta
-bound.ml.L = ml.mat[,1]
-bound.ml.U = ml.mat[1,]
+bound.ml.L <- ml.mat[,1]
+bound.ml.U <- ml.mat[1,]
+bound.prop.L <- prop.mat[,1]
+bound.prop.U <- prop.mat[1,]
+bound.prog.L <- prog.mat[,1]
+bound.prog.U <- prog.mat[1,]
+bound.block.L <- block.mat[,1]
+bound.block.U <- block.mat[1,]
 
 ## Plot
-jpeg("Bound.jpeg",width=6, height = 4.5, units='in',res=1200)
-plot(delta.seq,bound.ml.L,type="l",col=1, ylim = c(-0.15, 0.35), xlab = TeX(r'($\delta$)'), ylab = "Average Treatment Effect (ATE)")
+# png("Bound1.png", width = 6, height = 5.7, units='in', res = 1200)
+plot(delta.seq,bound.ml.L,type="l",col=1, ylim = c(-0.15, 0.37), xlab = TeX(r'($\delta$)'), ylab = "Average Treatment Effect (ATE)")
 lines(delta.seq,bound.ml.U,type="l",col=1)
 lines(delta.seq,bound.prop.L,type="l",lty = 2)
 lines(delta.seq,bound.prop.U,type="l",lty = 2)
@@ -151,21 +120,45 @@ lines(delta.seq,bound.prog.U,type="l",lty = 3)
 lines(delta.seq,bound.block.L,type="l",lty = 4)
 lines(delta.seq,bound.block.U,type="l",lty = 4)
 lines(delta.seq,rep(0,length(delta.seq)),lwd = 2, lty =1, col="red")
-legend("topright",c("ML","Prop","Prog","Block"), lwd = 1, lty = c(1,2,3,4),cex = 0.5)
-dev.off()
+legend("topright",c("ML","Prop","Prog","Block"), lwd = 1, lty = c(1,2,3,4),cex = 0.8)
+# dev.off()
 ########################################################
 
 ########################################################
 ### Figure 2
 
+## Bounds for the ATE with eta0 <= eta1
+bound.ml.L <- cummin(diag(ml.mat))
+bound.prop.L <- cummin(diag(prop.mat))
+bound.prog.L <- cummin(diag(prog.mat))
+bound.block.L <- cummin(diag(block.mat))
+
+## Plot
+# png("Bound2.png", width = 6, height = 5.7, units='in', res = 1200)
+plot(delta.seq,bound.ml.L,type="l",col=1, ylim = c(-0.05, 0.35), xlab = TeX(r'($\delta$)'), ylab = "Average Treatment Effect (ATE)")
+lines(delta.seq,bound.ml.U,type="l",col=1)
+lines(delta.seq,bound.prop.L,type="l",lty = 2)
+lines(delta.seq,bound.prop.U,type="l",lty = 2)
+lines(delta.seq,bound.prog.L,type="l",lty = 3)
+lines(delta.seq,bound.prog.U,type="l",lty = 3)
+lines(delta.seq,bound.block.L,type="l",lty = 4)
+lines(delta.seq,bound.block.U,type="l",lty = 4)
+lines(delta.seq,rep(0,length(delta.seq)),lwd = 2, lty =1, col="red")
+legend("topright",c("ML","Prop","Prog","Block"), lwd = 1, lty = c(1,2,3,4),cex = 0.8)
+# dev.off()
+########################################################
+
+########################################################
+### Figure 3
+
 ## Point estimates of the ATE
-jpeg("ATE.jpeg",width=6, height = 4.5, units='in',res=1200)
-plot(eta0.seq, diag(ml.mat), type = "l", ylim = c(0.05,0.12), xlab = TeX(r'($\eta_0 = \eta_1$)'),ylab = "Average Treatment Effect (ATE)", lty = 1)
+# png("ATE.png", width = 6, height = 5.7, units='in', res = 1200)
+plot(eta0.seq, diag(ml.mat), type = "l", ylim = c(0.05,0.13), xlab = TeX(r'($\eta_0 = \eta_1$)'),ylab = "Average Treatment Effect (ATE)", lty = 1)
 lines(eta0.seq, diag(prop.mat), type = "l", lty = 2)
 lines(eta0.seq, diag(prog.mat), type = "l", lty = 3)
 lines(eta0.seq, diag(block.mat), type = "l", lty = 4)
-legend("topright",c("ML","Prop","Prog","Block"),lwd = 1, lty=c(1,2,3,4),cex = 0.5)
-dev.off()
+legend("topright",c("ML","Prop","Prog","Block"),lwd = 1, lty=c(1,2,3,4),cex = 0.8)
+# dev.off()
 ########################################################
 
 
@@ -174,83 +167,87 @@ dev.off()
 
 ## Calculating bootstrap confidence intervals
 library(doParallel)
-eta.seq = seq(0, 0.5, by = 0.1)
-nsim = 500
-strata = 20
-bsize = 50
+eta.seq <- seq(0, 0.5, by = 0.1)
+nsim <- 500
+strata <- 20
+bsize <- 50
 
 cl <- parallel::makeCluster(10) # need to specify the number of cores to use. 
 doParallel::registerDoParallel(cl)
 
 ## Calculating bootstrap standard deviations
-boot.mat = foreach(k = 1:nsim, .combine = "rbind") %dopar% {
-  N = dim(newdata)[1]
-  sample.index = sample(1:N, N, replace = T)
-  boot.data = newdata[sample.index, ]
+boot.mat <- foreach(k = 1:nsim, .combine = "rbind") %dopar% {
+  N <- dim(newdata)[1]
+  sample.index <- sample(1:N, N, replace = T)
+  boot.data <- newdata[sample.index, ]
   
   ## Bootstrap propensity stratification
-  prop.score.model = glm(anger75 ~ abuse + female + marital.prob + age + edufa + edumo + log.income + rural, data = boot.data, family = "binomial", x = TRUE)
-  prop.score.star = prop.score.model$fitted.values
-  boot.prop.mat = SP.set(boot.data, strata + 1, prop.score.star)
+  prop.score.model <- glm(anger75 ~ abuse + female + marital.prob + age + edufa + edumo + log.income + rural, data = boot.data, family = "binomial", x = TRUE)
+  prop.score.star <- prop.score.model$fitted.values
+  boot.prop.mat <- SP.set(boot.data, strata + 1, prop.score.star)
   
   ## Bootstrap prognostic stratification
-  newdata_ts = boot.data[boot.data$abuse==1,]
-  prog.score.model = glm(anger75 ~ abuse + female + marital.prob + age + edufa + edumo + log.income + rural, data = newdata_ts, family = "binomial", x = TRUE)
-  gamma_x = prog.score.model$coefficients[-c(1,2)]
-  prog.score.star = as.matrix(boot.data[,-c(1,2)]) %*% as.vector(gamma_x)
-  boot.prog.mat = SP.set(boot.data, strata + 1, prog.score.star)
+  newdata_ts <- boot.data[boot.data$abuse==1,]
+  prog.score.model <- glm(anger75 ~ abuse + female + marital.prob + age + edufa + edumo + log.income + rural, data = newdata_ts, family = "binomial", x = TRUE)
+  gamma_x <- prog.score.model$coefficients[-c(1,2)]
+  prog.score.star <- as.matrix(boot.data[,-c(1,2)]) %*% as.vector(gamma_x)
+  boot.prog.mat <- SP.set(boot.data, strata + 1, prog.score.star)
   
   ## Bootstrap blocking
-  boot.block.mat = Block.set(boot.data, bsize = bsize)
+  boot.block.mat <- Block.set(boot.data, bsize = bsize)
   
   ## Bootstrap estimators
   boot.ML.est = boot.Prop.est = boot.Prog.est = boot.Block.est = rep(NA, length(eta.seq))
   for(j in 1:length(eta.seq)){
     eta.val = eta.seq[j]
-    boot.ML.est[j] = ML.logistic.under(data = boot.data, eta = c(eta.val, eta.val))$risk.diff
-    boot.Prop.est[j] = SP.inference.under(count.mat = boot.prop.mat, eta = c(eta.val, eta.val))$risk.diff
-    boot.Prog.est[j] = SP.inference.under(count.mat = boot.prog.mat, eta = c(eta.val, eta.val))$risk.diff
-    boot.Block.est[j] = Block.inference.under(count.mat = boot.block.mat, eta = c(eta.val, eta.val))$risk.diff
+    boot.ML.est[j] <- ML.logistic.under(data = boot.data, eta = c(eta.val, eta.val))$risk.diff
+    boot.Prop.est[j] <- SP.inference.under(count.mat = boot.prop.mat, eta = c(eta.val, eta.val))$risk.diff
+    boot.Prog.est[j] <- SP.inference.under(count.mat = boot.prog.mat, eta = c(eta.val, eta.val))$risk.diff
+    boot.Block.est[j] <- Block.inference.under(count.mat = boot.block.mat, eta = c(eta.val, eta.val))$risk.diff
   }
   c(boot.ML.est, boot.Prop.est, boot.Prog.est, boot.Block.est)
 }
 parallel::stopCluster(cl)
 
-boot.mat.ML = boot.mat[,1:length(eta.seq)]
-boot.mat.Prop = boot.mat[,(length(eta.seq)+1):(2*length(eta.seq))]
-boot.mat.Prog = boot.mat[,(2*length(eta.seq)+1):(3*length(eta.seq))]
-boot.mat.Block = boot.mat[,(3*length(eta.seq)+1):(4*length(eta.seq))]
+boot.mat.ML <- boot.mat[,1:length(eta.seq)]
+boot.mat.Prop <- boot.mat[,(length(eta.seq)+1):(2*length(eta.seq))]
+boot.mat.Prog <- boot.mat[,(2*length(eta.seq)+1):(3*length(eta.seq))]
+boot.mat.Block <- boot.mat[,(3*length(eta.seq)+1):(4*length(eta.seq))]
 
 ## Bootstrap standard deviations
-ML.sd = apply((boot.mat.ML), 2, sd)
-Prop.sd = apply((boot.mat.Prop), 2, sd)
-Prog.sd = apply((boot.mat.Prog), 2, sd)
-Block.sd = apply((boot.mat.Block), 2, sd)
+ML.sd <- apply((boot.mat.ML), 2, sd)
+Prop.sd <- apply((boot.mat.Prop), 2, sd)
+Prog.sd <- apply((boot.mat.Prog), 2, sd)
+Block.sd <- apply((boot.mat.Block), 2, sd)
 
 ## Estimators
-ML.est = diag(ml.mat)[seq(from = 1, by = 10, length = 6)]
-Prop.est = diag(prop.mat)[seq(from = 1, by = 10, length = 6)]
-Prog.est = diag(prog.mat)[seq(from = 1, by = 10, length = 6)]
-Block.est = diag(block.mat)[seq(from = 1, by = 10, length = 6)]
+ML.est <- diag(ml.mat)[seq(from = 1, by = 10, length = 6)]
+Prop.est <- diag(prop.mat)[seq(from = 1, by = 10, length = 6)]
+Prog.est <- diag(prog.mat)[seq(from = 1, by = 10, length = 6)]
+Block.est <- diag(block.mat)[seq(from = 1, by = 10, length = 6)]
 ########################################################
 
 
 ########################################################
-### Figure 3
+### Figure 4
 
 ## Contour plots for the ATE
-jpeg("ATE_ML.jpeg",width=6, height = 4.5, units='in',res=1200)
+# ATE_ML
+# png("ATE_ML.png", width = 6, height = 5.7, units='in', res = 1200)
 contour(eta0.seq, eta1.seq, ml.mat, xlab = TeX(r'($\eta_0$)'), ylab = TeX(r'($\eta_1$)'), nlevels=20)
-dev.off()
-jpeg("ATE_Prop.jpeg",width=6, height = 4.5, units='in',res=1200)
+# dev.off()
+# ATE_Prop
+# png("ATE_Prop.png", width = 6, height = 5.7, units='in', res = 1200)
 contour(eta0.seq, eta1.seq, prop.mat, xlab = TeX(r'($\eta_0$)'), ylab = TeX(r'($\eta_1$)'), nlevels=20)
-dev.off()
-jpeg("ATE_Prog.jpeg",width=6, height = 4.5, units='in',res=1200)
+# dev.off()
+# ATE_Prog
+# png("ATE_Prog.png", width = 6, height = 5.7, units='in', res = 1200)
 contour(eta0.seq, eta1.seq, prog.mat, xlab = TeX(r'($\eta_0$)'), ylab = TeX(r'($\eta_1$)'), nlevels=20)
-dev.off()
-jpeg("ATE_Block.jpeg",width=6, height = 4.5, units='in',res=1200)
+# dev.off()
+# ATE_Block
+# png("ATE_Block.png", width = 6, height = 5.7, units='in', res = 1200)
 contour(eta0.seq, eta1.seq, block.mat, xlab = TeX(r'($\eta_0$)'), ylab = TeX(r'($\eta_1$)'), nlevels=20)
-dev.off()
+# dev.off()
 ########################################################
 
 
